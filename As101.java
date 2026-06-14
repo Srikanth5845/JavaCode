9996771df7236d339e9070211749e9cb83c0ef26
Srikanth <srikanth5845@gmail.com>
2026-06-14 17:52:38 +0530

    assignment

---
+++ As101.java
@@ -0,0 +1,22 @@
+package assignments;
+
+public class As101 {
+
+	public static void main(String[] args) {
+		//byte to int
+		
+		byte a =100;
+		
+		int b =a;
+		b=100000;
+		System.out.println(b);
+		
+		//long to byte
+		
+		long s=145647l;
+		byte d =(byte)s;
+		System.out.println(d);
+
+	}
+
+}
